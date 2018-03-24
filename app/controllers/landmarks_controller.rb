afa1a6196523ea8a9ebb986af8b02b42327278e2
Robert Hughes <roberts.ginger.email@gmail.com>
2018-03-24 05:14:37 +0000

    show individual landmarks, show all landmarks, create new landmarks

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,20 @@
 class LandmarksController < ApplicationController
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+  end
 
+  post '/landmarks' do
+    landmark = Landmark.create(params[:landmark])
+    redirect "/landmarks/#{landmark.id}"
+  end
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'landmarks/index'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/show'
+  end
 end
